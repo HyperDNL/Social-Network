a79179837a357faf6cd48895446e3fe12617e59d
Daniel López <hyperdaniel579exe@gmail.com>
2023-07-30 12:19:05 -0600

    Global variables setup from environment variables

---
+++ backend/src/config/config.js
@@ -0,0 +1,13 @@
+import { config } from "dotenv";
+config();
+
+export const PORT = process.env.PORT;
+export const MONGO_DB_URI = process.env.MONGO_DB_URI;
+export const JWT_SECRET = process.env.JWT_SECRET;
+export const SESSION_EXPIRY = process.env.SESSION_EXPIRY;
+export const REFRESH_TOKEN_EXPIRY = process.env.REFRESH_TOKEN_EXPIRY;
+export const COOKIE_SECRET = process.env.COOKIE_SECRET;
+export const WHITELISTED_DOMAINS = process.env.WHITELISTED_DOMAINS;
+export const CLOUD_NAME = process.env.CLOUD_NAME;
+export const API_KEY = process.env.API_KEY;
+export const API_SECRET = process.env.API_SECRET;
