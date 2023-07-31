becc28a1fee3c8c0a953557a789fb4443b519cd4
Daniel López <hyperdaniel579exe@gmail.com>
2023-07-31 13:53:39 -0600

    Updated global variables

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -11,3 +11,7 @@ export const WHITELISTED_DOMAINS = process.env.WHITELISTED_DOMAINS;
 export const CLOUD_NAME = process.env.CLOUD_NAME;
 export const API_KEY = process.env.API_KEY;
 export const API_SECRET = process.env.API_SECRET;
+export const MAX_IMAGE_SIZE = process.env.MAX_IMAGE_SIZE;
+export const ALLOWED_IMAGE_EXTENSIONS = JSON.parse(
+  process.env.ALLOWED_IMAGE_EXTENSIONS
+);
